8ad1cd7c618fd7b1c81650b75da5d7e57951bf06
dezem <dezem@users.noreply.github.com>
2021-09-28 19:23:36 +0200

    big thx to R-YaTian for Sparse Title install like NMH3, Last stop, Lost in random and qazrfv1234 for zh-TW lang update

--- include/install/nca.hpp
+++ include/install/nca.hpp
@@ -6,10 +6,27 @@
 #define MAGIC_NCA3 0x3341434E /* "NCA3" */
 namespace tin::install
 {
+    struct NcaBucketInfo {
+        static constexpr size_t HeaderSize = 0x10;
+        s64 offset;
+        s64 size;
+        u8 header[HeaderSize];
+    };
+
+    static_assert(sizeof(NcaBucketInfo) == 0x20, "NcaBucketInfo must be 0x20");
+
+    struct NcaSparseInfo {
+        NcaBucketInfo bucket;
+        s64 physical_offset;
+        u16 generation;
+        u8  reserved[6];
+    };
+
+    static_assert(sizeof(NcaSparseInfo) == 0x30, "NcaSparseInfo must be 0x30");
+
     struct NcaFsHeader
     {
-        u8 _0x0;
-        u8 _0x1;
+        u16 version;
         u8 partition_type;
         u8 fs_type;
         u8 crypt_type;
@@ -28,7 +45,8 @@ namespace tin::install
                 u32 section_ctr_high;
             };
         };
-        u8 _0x148[0xB8]; /* Padding. */
+        NcaSparseInfo sparse_info; /* only used in sections with sparse storage. */
+        u8 _0x178[0x88]; /* Padding. */
     } PACKED;
 
     static_assert(sizeof(NcaFsHeader) == 0x200, "NcaFsHeader must be 0x200");
@@ -53,7 +71,7 @@ namespace tin::install
         u8 m_kaekIndex; /* Which kaek index? */
         u64 nca_size; /* Entire archive size. */
         u64 m_titleId;
-        u8 _0x218[0x4]; /* Padding. */
+        u32 m_contentIndex;
         union {
             uint32_t sdk_version; /* What SDK was this built with? */
             struct {
@@ -64,7 +82,8 @@ namespace tin::install
             };
         };
         u8 m_cryptoType2; /* Which keyblob (field 2) */
-        u8 _0x221[0xF]; /* Padding. */
+        u8 m_cryptoType3;
+        u8 _0x222[0xE]; /* Padding. */
         u64 m_rightsId[2]; /* Rights ID (for titlekey crypto). */
         NcaSectionEntry section_entries[4]; /* Section entry metadata. */
         u8 section_hashes[4 * 0x20]; /* SHA-256 hashes for each section header. */
