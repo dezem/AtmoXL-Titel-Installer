fa2c4197d79b57cf25183600e8bf5c7c2ce3d94f
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-04-06 11:39:34 +0800

    Update install_nsp.cpp

--- source/install/install_nsp.cpp
+++ source/install/install_nsp.cpp
@@ -69,7 +69,8 @@ namespace tin::install::nsp
 
             NcmContentInfo cnmtContentInfo;
             cnmtContentInfo.content_id = cnmtContentId;
-            *(u64*)&cnmtContentInfo.size = cnmtNcaSize & 0xFFFFFFFFFFFF;
+            ncmU64ToContentInfoSize(cnmtNcaSize & 0xFFFFFFFFFFFF, &cnmtContentInfo);
+            //*(u64*)&cnmtContentInfo.size = cnmtNcaSize & 0xFFFFFFFFFFFF;
             cnmtContentInfo.content_type = NcmContentType_Meta;
 
             CNMTList.push_back( { tin::util::GetContentMetaFromNCA(cnmtNCAFullPath), cnmtContentInfo } );
