5c78b9f9366f6ba02539bfe681d02ff47199144d
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-04-06 11:40:05 +0800

    Update install_xci.cpp

--- source/install/install_xci.cpp
+++ source/install/install_xci.cpp
@@ -65,7 +65,8 @@ namespace tin::install::xci
 
             NcmContentInfo cnmtContentInfo;
             cnmtContentInfo.content_id = cnmtContentId;
-            *(u64*)&cnmtContentInfo.size = cnmtNcaSize & 0xFFFFFFFFFFFF;
+            ncmU64ToContentInfoSize(cnmtNcaSize & 0xFFFFFFFFFFFF, &cnmtContentInfo);
+            //*(u64*)&cnmtContentInfo.size = cnmtNcaSize & 0xFFFFFFFFFFFF;
             cnmtContentInfo.content_type = NcmContentType_Meta;
 
             CNMTList.push_back( { tin::util::GetContentMetaFromNCA(cnmtNCAFullPath), cnmtContentInfo } );
